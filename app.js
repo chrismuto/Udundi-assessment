1cf7de664b2d788310280df2fa55d391a5b344cc
unknown <chrismuto@gmail.com>
2024-02-16 11:34:42 -0800

    added transition for modal on button click

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const header = document.getElementById("header")
 var modal = document.getElementById("modal")
 
 function openModal() {
-    modal.style.display = "block"
+    modal.classList.remove("scale-0")
+    modal.classList.add("scale-1")
     header.style.display = "none"
     button.style.display = "none"
 }
 
 function closeModal() {
-    modal.style.display = "none"
+    modal.classList.add("scale-0")
+    modal.classList.remove("scale-1")
     header.style.display = "block"
     button.style.display = "block"
 }
